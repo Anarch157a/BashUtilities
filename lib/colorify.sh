4d9c156f2813e54b043e07bc3ca0471dabae9cb7
Nerdeiro da Silva <Anarch157a@users.noreply.github.com>
2017-04-17 10:16:31 -0300

    created usage and fixed bugs
    Created usage help and fixed a small bug in pprint() that prevented the function from being called directly.

--- lib/colorify.sh
+++ lib/colorify.sh
@@ -70,14 +70,86 @@ pprint() {
                 ;;
         esac
     done
-    echo $STR
+    echo -e "${STR}"
 }
+
+usage(){
+echo '
+To use pprint function in your scripts, source it with:
+
+source colorify.sh
+
+or
+
+. colorify.sh
+
+It can also be used interactivelly from the command line. Just run it like:
+
+$ colorify.sh "String to be formated"
+
+When calling the function or running from an interactive shell, the strings can
+be formatted using the following codes:
+
+%%RS = Reset attributes. Everything after will be printed in the default colors
+%%BO = Activates bold attribute
+%%IT = Turns italics on (if supported by the terminal and font)
+%%UN = Underlines text (if supported by the terminal and font)
+%%FL = Flashing/blinking text
+%%IN = Inverted. Inverts foreground and background colors
+
+%%BK = Black
+%%RE = Red
+%%GR = Green
+%%YE = Yellow
+%%BL = Blue
+%%MA = Magenta
+%%CY = Cyan
+%%WH = White
+
+Example:
+
+From command line:
+
+$ colorify.sh "%%FLThis is a blinking text.%%RS"
+
+Will print a flashing string like:
+'
+
+pprint "%%FLThis is a blinking text.%%RS"
+
+echo '
+From a script:
+
+source colorify.sh
+
+pprint "%%REThis text is %%BLblue%%RS"
+
+Will print:
+'
+
+pprint "%%REThis text is %%BLblue%%RS"
+
+echo '
+Remember to always end your strings with the reset code %%RS. Otherwise everything
+printed aftwards with in the terminal will still be using the last attributes set
+by the function.
+'
+}
+
+#
+# Here is the interactive part
+#
 
 if [ "${BASH_SOURCE[0]}" != "${0}" ]
 then
     return 0
 fi
 
+if [[ -z "$@" ]]
+then
+    usage
+    exit 0
+fi
 PARAMS="$@"
 
 printf "$(pprint $PARAMS)\n"
